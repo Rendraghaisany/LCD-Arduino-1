f2f8ae28cb632a2ca6d1c8283a55c81fd06093af
Rendra Ghaisany Althaf <ghaisanyrendra@gmail.com>
2025-02-22 11:07:11 +0700

    update main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <LiquidCrystal_I2C.h>
+
+int kolom = 16;
+int baris = 2;
+
+LiquidCrystal_I2C lcd(0x27, kolom, baris);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+lcd.init();
+lcd.backlight();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+int i;
+lcd.setCursor(1,0);
+lcd.print("Latihan Arduino Simulasi LCD I2CL");
+
+lcd.setCursor(0,1);
+lcd.print("Sekolah BQ Islamic Boarding School");
+
+for (i = 0; i<16; i++){
+  lcd.scrollDisplayLeft();
+  delay(500);
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
 }
+
